985cc2f8b599d1707e1a1ce8d460018813be9bdf
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-11-10 20:38:08 +0100

    Add image endpoint

---
+++ src/routes/(authed)/api/image/[albumId]/+server.ts
@@ -0,0 +1,36 @@
+import prisma from '$lib/server/prisma.js';
+import { error } from '@sveltejs/kit';
+import { readFile } from 'fs/promises';
+
+export const GET = async ({ params, setHeaders }) => {
+	const { albumId } = params;
+
+	console.log('albumId', albumId);
+
+	const album = await prisma.album.findUnique({
+		where: {
+			id: albumId
+		}
+	});
+
+	if (!album) {
+		throw error(404, { message: 'Album not found' });
+	}
+
+	if (album.albumArt) {
+		try {
+			const image = await readFile(album.albumArt);
+			const imageType = album.albumArt.split('.').pop();
+
+			setHeaders({
+				'Content-Type': `image/${imageType}`
+			});
+			return new Response(image);
+		} catch (err) {
+			console.log(err);
+			throw error(500, { message: 'Internal server error' });
+		}
+	} else {
+		throw error(404, { message: 'Album art not found' });
+	}
+};
