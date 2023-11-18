ce373e381045979120cc67c146acfa6dc9b1362c
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-11-18 20:13:03 +0100

    Add unplugin-icons to Vite config and package.json

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-
+import 'unplugin-icons/types/svelte';
 import type { SignedInUser } from '$lib/shared/types';
+
 declare global {
 	namespace App {
 		// interface Error {}
