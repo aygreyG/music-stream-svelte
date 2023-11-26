6d002c34484a20c70e9126305114ee5015972967
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-11-26 16:56:51 +0100

    Add navigation elements to NavBar component

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
 	import type { SignedInUser } from '$lib/shared/types';
-	import { queue } from '$lib/stores/audioPlayer';
+	import { previous, queue } from '$lib/stores/audioPlayer';
+	import NavigationElement from './NavigationElement.svelte';
 
 	export let user: SignedInUser | null = null;
+
+	type NavigationElement = {
+		href: string;
+		text: string;
+	};
+
+	const navigationElements: NavigationElement[] = [
+		{
+			href: '/',
+			text: 'Home'
+		}
+	];
+
+	const loggedInElements: NavigationElement[] = [
+		{
+			href: '/dashboard',
+			text: 'Dashboard'
+		},
+		{
+			href: '/logout',
+			text: 'Logout'
+		}
+	];
+
+	const loggedOutElements: NavigationElement[] = [
+		{
+			href: '/login',
+			text: 'Login'
+		}
+	];
 </script>
 
 <div class="w-4/12 xl:w-3/12 sm:flex flex-col h-full gap-1 hidden">
@@ -11,17 +43,31 @@
 		{#if user}
 			<p class="font-bold">Welcome {user.username}</p>
 		{/if}
-		<a href="/">Home</a>
+		{#each navigationElements as el}
+			<NavigationElement {...el} />
+		{/each}
 		{#if user}
-			<a href="/dashboard">Dashboard</a>
-			<form method="POST" action="/logout" use:enhance>
-				<button type="submit">Logout</button>
-			</form>
+			{#each loggedInElements as el}
+				{#if el.href === '/logout'}
+					<form method="POST" action="/logout" use:enhance>
+						<button type="submit">{el.text}</button>
+					</form>
+				{:else}
+					<NavigationElement {...el} />
+				{/if}
+			{/each}
 		{:else}
-			<a href="/login">Login</a>
+			{#each loggedOutElements as el}
+				<NavigationElement {...el} />
+			{/each}
 		{/if}
 	</div>
 	<div class="bg-zinc-900/95 p-2 rounded-md min-h-fit h-full overflow-auto">
+		{#each $previous as { album, track }}
+			<div>
+				{track.artists.map((a) => a.name).join(', ')} - {track.title}
+			</div>
+		{/each}
 		{#each $queue as { album, track }}
 			<div>
 				{track.artists.map((a) => a.name).join(', ')} - {track.title}
---
+++ src/lib/components/NavigationElement.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string;
+	export let text: string;
+
+	$: currentPage = $page.url.pathname;
+</script>
+
+<div>
+	<a class="transition-colors" class:text-fuchsia-600={href === currentPage} {href}>{text}</a>
+</div>
