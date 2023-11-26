8733a40cce81a11a6c7a9386dd94cf3fb01e7c19
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-11-26 16:57:13 +0100

    Add previous track functionality to audio player, update player functions

--- src/lib/components/AudioPlayer.svelte
+++ src/lib/components/AudioPlayer.svelte
@@ -10,12 +10,11 @@
 	import RoundRepeat from 'virtual:icons/ic/round-repeat';
 	import RoundShuffle from 'virtual:icons/ic/round-shuffle';
 	import type { SignedInUser } from '$lib/shared/types';
-	import { currentTrack, playNext } from '$lib/stores/audioPlayer';
+	import { currentTrack, playNext, paused, playPrevious } from '$lib/stores/audioPlayer';
 
 	export let user: SignedInUser | null = null;
 
 	let player: HTMLAudioElement;
-	let paused = true;
 	let currentTime = 0;
 	let duration: number;
 	let volume = 0.1;
@@ -29,13 +28,17 @@
 		if (!$currentTrack) {
 			return;
 		}
-		if (paused) {
-			player.pause();
-		} else {
-			player.play();
-		}
 
-		paused = !paused;
+		$paused = !$paused;
+	}
+
+	function onEnded() {
+		if (repeat) {
+			player.currentTime = 0;
+			player.play();
+		} else {
+			playNext();
+		}
 	}
 
 	function updateVolume(e: WheelEvent) {
@@ -59,137 +62,144 @@
 
 	currentTrack.subscribe((val) => {
 		if (val && !val.shouldBePlayed) {
-			paused = true;
+			$paused = true;
 		}
 	});
 </script>
 
-<div class="h-full p-2">
-	{#if user}
-		<div class="flex h-full w-full gap-4 px-2">
-			<div class="sm:flex hidden w-40 h-full justify-start items-center">
-				<div class="h-40 w-40 overflow-hidden rounded-md bg-zinc-600/50">
-					{#if $currentTrack}
-						<audio
-							preload="metadata"
-							src="/api/play/{$currentTrack.track.id}"
-							bind:currentTime
-							bind:duration
-							bind:paused
-							bind:this={player}
-							bind:volume
-							autoplay={$currentTrack.shouldBePlayed}
-						/>
-						<div
-							class="flex flex-col absolute overflow-hidden z-10 bg-zinc-900/80 text-center bottom-0 left-0 w-full"
-						>
-							<a
-								href="/album/{$currentTrack.album.id}"
-								class="whitespace-nowrap text-xl font-bold hover:underline"
-							>
-								{$currentTrack.track.title}
-							</a>
-							<a
-								href="/artist/{$currentTrack.track.artists[0].id}"
-								class="whitespace-nowrap hover:underline"
-							>
-								{$currentTrack.track.artists.map((a) => a.name).join(', ')}
-							</a>
-						</div>
-						<a href="/album/{$currentTrack.album.id}">
-							<img
-								class="object-cover rounded-md"
-								src="/api/image/{$currentTrack.album.id}"
-								alt="Album Art"
-							/>
-						</a>
-					{/if}
-				</div>
-			</div>
-			<div class="flex flex-col justify-around h-full w-full">
-				<div class="flex justify-between gap-2 items-center whitespace-nowrap sm:order-2">
-					<div class="timer">{currentString}</div>
-					<input
-						class="w-full"
-						type="range"
-						min="0"
-						max={duration}
-						step="0.01"
-						bind:value={currentTime}
+<div class="h-full flex gap-2 w-full">
+	<div class="hidden sm:block">
+		<div class="overflow-hidden h-44 w-44 rounded-md bg-zinc-900/95">
+			{#if $currentTrack}
+				<audio
+					preload="metadata"
+					src="/api/play/{$currentTrack.track.id}"
+					bind:currentTime
+					bind:duration
+					bind:paused={$paused}
+					bind:this={player}
+					bind:volume
+					autoplay={$currentTrack.shouldBePlayed}
+					on:ended={onEnded}
+				/>
+				<a href="/album/{$currentTrack.album.id}">
+					<img
+						class="object-cover rounded-md w-full h-full"
+						src="/api/image/{$currentTrack.album.id}"
+						alt="Album Art"
 					/>
-					<div class="timer">{durationString}</div>
-				</div>
-				<div class="flex gap-2 justify-center sm:justify-around lg:justify-center items-center">
-					<div class="flex gap-4">
-						<button
-							on:click={() => (shuffle = !shuffle)}
-							class="text-2xl transition-colors"
-							class:text-fuchsia-600={shuffle}
-							class:text-zinc-400={!shuffle}
-						>
-							<RoundShuffle />
-						</button>
-						<button
-							on:click={playNext}
-							class="text-3xl text-zinc-400 active:text-zinc-600 transition-colors"
-						>
-							<RoundSkipPrevious />
-						</button>
-						<button
-							on:click={togglePlay}
-							class="text-6xl text-zinc-400 active:text-zinc-600 transition-colors"
-						>
-							{#if paused}
-								<RoundPlayCircleOutline />
-							{:else}
-								<RoundPauseCircleOutline />
-							{/if}
-						</button>
-						<button
-							on:click={playNext}
-							class="text-3xl text-zinc-400 active:text-zinc-600 transition-colors"
-						>
-							<RoundSkipNext />
-						</button>
-						<button
-							on:click={() => (repeat = !repeat)}
-							class="text-2xl transition-colors"
-							class:text-fuchsia-600={repeat}
-							class:text-zinc-400={!repeat}
-						>
-							<RoundRepeat />
-						</button>
-					</div>
 					<div
-						class="sm:flex hidden gap-2 items-center lg:absolute right-0"
-						on:mousewheel={updateVolume}
+						class="bottom-0 left-0 absolute w-full text-center flex justify-end flex-col gap-1 p-1"
 					>
-						<button
-							on:click={() => {
-								if (volume === 0) {
-									volume = prevVolume;
-								} else {
-									prevVolume = volume;
-									volume = 0;
-								}
-							}}
+						<a
+							href="/album/{$currentTrack.album.id}"
+							class="whitespace-nowrap z-10 text-ellipsis overflow-hidden bg-zinc-900/80 backdrop-blur-sm rounded-md px-1"
 						>
-							{#if volume === 0}
-								<RoundVolumeOff class="text-3xl text-zinc-400" />
-							{:else if volume < 0.3}
-								<RoundVolumeMute class="text-3xl text-zinc-400" />
-							{:else if volume < 0.7}
-								<RoundVolumeDown class="text-3xl text-zinc-400" />
-							{:else}
-								<RoundVolumeUp class="text-3xl text-zinc-400" />
-							{/if}
-						</button>
-						<input type="range" min="0" max="1" step="0.01" bind:value={volume} />
+							{$currentTrack.track.title}
+						</a>
+						<div
+							class="whitespace-nowrap z-10 text-ellipsis overflow-hidden bg-zinc-900/80 backdrop-blur-sm rounded-md text-xs px-1"
+						>
+							{#each $currentTrack.track.artists.sort( (a, b) => (a.name !== $currentTrack?.album.albumArtist.name ? 1 : -1) ) as artist, index (artist.id)}
+								<a class="hover:underline" href="/artist/{artist.id}">
+									{artist.name}{#if $currentTrack.track.artists.length > 1 && index != $currentTrack.track.artists.length - 1},{/if}
+								</a>
+							{/each}
+						</div>
+					</div>
+				</a>
+			{/if}
+		</div>
+	</div>
+	<div class="h-full p-2 w-full bg-zinc-900/95 rounded-md">
+		{#if user}
+			<div class="flex h-full w-full gap-4 px-2">
+				<div class="flex flex-col justify-around h-full w-full">
+					<div class="flex justify-between gap-2 items-center whitespace-nowrap sm:order-2">
+						<div class="timer">{currentString}</div>
+						<input
+							class="w-full"
+							type="range"
+							min="0"
+							max={duration}
+							step="0.01"
+							bind:value={currentTime}
+							disabled={!duration}
+						/>
+						<div class="timer">{durationString}</div>
+					</div>
+					<div class="flex gap-2 justify-center sm:justify-around lg:justify-center items-center">
+						<div class="flex gap-4">
+							<button
+								on:click={() => (shuffle = !shuffle)}
+								class="text-2xl transition-colors"
+								class:text-fuchsia-600={shuffle}
+								class:text-zinc-400={!shuffle}
+							>
+								<RoundShuffle />
+							</button>
+							<button
+								on:click={playPrevious}
+								class="text-3xl text-zinc-400 active:text-zinc-600 transition-colors"
+							>
+								<RoundSkipPrevious />
+							</button>
+							<button
+								on:click={togglePlay}
+								class="text-6xl text-zinc-400 active:text-zinc-600 transition-colors"
+							>
+								{#if $paused}
+									<RoundPlayCircleOutline />
+								{:else}
+									<RoundPauseCircleOutline />
+								{/if}
+							</button>
+							<button
+								on:click={playNext}
+								class="text-3xl text-zinc-400 active:text-zinc-600 transition-colors"
+							>
+								<RoundSkipNext />
+							</button>
+							<button
+								on:click={() => (repeat = !repeat)}
+								class="text-2xl transition-colors"
+								class:text-fuchsia-600={repeat}
+								class:text-zinc-400={!repeat}
+							>
+								<RoundRepeat />
+							</button>
+						</div>
+						<div
+							class="sm:flex hidden gap-2 items-center lg:absolute right-0"
+							on:mousewheel={updateVolume}
+						>
+							<button
+								on:click={() => {
+									if (volume === 0) {
+										volume = prevVolume;
+									} else {
+										prevVolume = volume;
+										volume = 0;
+									}
+								}}
+							>
+								{#if volume === 0}
+									<RoundVolumeOff class="text-3xl text-zinc-400" />
+								{:else if volume < 0.3}
+									<RoundVolumeMute class="text-3xl text-zinc-400" />
+								{:else if volume < 0.7}
+									<RoundVolumeDown class="text-3xl text-zinc-400" />
+								{:else}
+									<RoundVolumeUp class="text-3xl text-zinc-400" />
+								{/if}
+							</button>
+							<input type="range" min="0" max="1" step="0.01" bind:value={volume} />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	{/if}
+		{/if}
+	</div>
 </div>
 
 <style lang="postcss">
--- src/lib/stores/audioPlayer.ts
+++ src/lib/stores/audioPlayer.ts
@@ -8,15 +8,25 @@ type TrackType = Prisma.TrackGetPayload<{ include: { artists: true } }>;
 
 export const queue = writable<{ track: TrackType; album: AlbumType }[]>([]);
 
+export const previous = writable<{ track: TrackType; album: AlbumType }[]>([]);
+
 export const currentTrack = writable<{
 	track: TrackType;
 	album: AlbumType;
 	shouldBePlayed: boolean;
 } | null>(null);
 
+export const paused = writable(true);
+
 export async function playTrack(track: TrackType, album: AlbumType, shouldBePlayed = true) {
 	if (shouldBePlayed) {
-		currentTrack.set({ track, album, shouldBePlayed });
+		currentTrack.update((t) => {
+			if (t) {
+				previous.update((p) => [...p, t]);
+			}
+
+			return { track, album, shouldBePlayed };
+		});
 	} else {
 		queue.update((q) => {
 			if (q.length === 0) {
@@ -32,12 +42,79 @@ export async function playTrack(track: TrackType, album: AlbumType, shouldBePlay
 export async function playNext() {
 	queue.update((q) => {
 		if (q.length === 0) {
+			currentTrack.update((t) => {
+				if (t) {
+					const nextTrack = t.album.tracks.find(
+						(track) => track.trackNumber === t.track.trackNumber + 1
+					);
+
+					if (nextTrack) {
+						previous.update((p) => [...p, t]);
+						return {
+							track: nextTrack,
+							album: t.album,
+							shouldBePlayed: true
+						};
+					}
+				}
+
+				return t;
+			});
+
 			return q;
 		}
 
 		const [next, ...rest] = q;
-		currentTrack.set({ track: next.track, album: next.album, shouldBePlayed: true });
+		currentTrack.update((t) => {
+			if (t) {
+				previous.update((p) => [...p, t]);
+			}
+
+			return {
+				track: next.track,
+				album: next.album,
+				shouldBePlayed: true
+			};
+		});
 		return rest;
+	});
+}
+
+export async function playPrevious() {
+	previous.update((p) => {
+		if (p.length === 0) {
+			currentTrack.update((t) => {
+				if (t) {
+					const previousTrack = t.album.tracks.find(
+						(track) => track.trackNumber === t.track.trackNumber - 1
+					);
+
+					if (previousTrack) {
+						return {
+							track: previousTrack,
+							album: t.album,
+							shouldBePlayed: true
+						};
+					}
+				}
+
+				return t;
+			});
+
+			return p;
+		}
+
+		const previousTrack = p.pop();
+
+		if (previousTrack) {
+			currentTrack.set({
+				track: previousTrack.track,
+				album: previousTrack.album,
+				shouldBePlayed: true
+			});
+		}
+
+		return p;
 	});
 }
 
