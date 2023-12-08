78ad859ebb9a840e60e922be6481ff6ccd78aa0d
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-12-08 22:41:15 +0100

    Format loading page Fixes #12

--- src/routes/loading/+page.svelte
+++ src/routes/loading/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
+  import RoundRefresh from 'virtual:icons/ic/round-refresh';
 
   onMount(() => {
-    const timeout = setTimeout(() => {
-      window.location.href = '/';
+    const timeout = setInterval(() => {
+      goto('/');
     }, 1000);
 
     return () => {
-      clearTimeout(timeout);
+      clearInterval(timeout);
     };
   });
 </script>
 
-<h1>The server is loading please wait...</h1>
+<div class="h-full w-full bg-zinc-900/95 rounded-md flex items-center justify-center text-2xl">
+  <RoundRefresh class="animate-spin w-8 h-8 ml-2" />
+  The server is loading please wait...
+</div>
