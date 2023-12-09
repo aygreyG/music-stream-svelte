b6587d626c5c40ab4632b30846658c3244f1f35e
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-12-09 18:52:11 +0100

    Format login page Fixes #24

--- src/routes/(app)/(unauthed)/login/+page.svelte
+++ src/routes/(app)/(unauthed)/login/+page.svelte
@@ -4,19 +4,37 @@
   export let data;
 </script>
 
-<div>
-  <h1>Login</h1>
+<div class="h-full flex flex-col items-center px-4">
+  <h1 class="text-xl text-center font-bold p-4">Login</h1>
 
-  <form method="POST" action="?&redirect_to={data.redirectTo ?? ''}" use:enhance>
-    <label>
-      Username
-      <input name="username" required />
+  <form
+    class="w-full max-w-lg flex flex-col gap-2 bg-zinc-600/10 rounded-md p-4"
+    method="POST"
+    action="?&redirect_to={data.redirectTo ?? ''}"
+    use:enhance
+  >
+    <label class="flex gap-1 flex-col">
+      <div class="text-zinc-400 font-bold text-sm">Username</div>
+      <input
+        class="bg-zinc-600 rounded-md px-2 py-1 transition-all outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-600"
+        name="username"
+        required
+      />
     </label>
-    <label>
-      Password
-      <input type="password" name="password" required />
+    <label class="flex flex-col gap-1">
+      <div class="text-zinc-400 font-bold text-sm">Password</div>
+      <input
+        class="bg-zinc-600 rounded-md px-2 py-1 transition-all outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-600"
+        type="password"
+        name="password"
+        required
+      />
     </label>
-
-    <button type="submit">Login</button>
+    <button
+      class="mt-2 self-center bg-fuchsia-600 hover:bg-fuchsia-700 rounded-md py-1 px-4 w-full font-semibold"
+      type="submit"
+    >
+      Login
+    </button>
   </form>
 </div>
