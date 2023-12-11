e79cc1d579bf49b7c4cf003e9a5fe8a8c285eb82
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-12-11 20:01:15 +0100

    Create an error page Fixes #17

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import RoundKeyboardBackspace from 'virtual:icons/ic/round-keyboard-backspace';
+</script>
+
+<div class="flex gap-4 flex-col items-center justify-center bg-zinc-900/95 h-full rounded-md">
+  <h1 class="text-2xl font-bold">Ooops, something went wrong...</h1>
+  <div class="flex flex-col items-center">
+    <div>Error code: <span class="font-bold text-fuchsia-600">{$page.status}</span></div>
+    {#if $page.error?.message}
+      <div>
+        Error message: <span class="font-bold text-fuchsia-600">{$page.error.message}</span>
+      </div>
+    {/if}
+  </div>
+  <a
+    class="group flex gap-1 items-center hover:text-fuchsia-600 focus-visible:text-fuchsia-600 transition-colors hover:bg-zinc-600/10 focus-visible:bg-zinc-600/10 p-2 rounded-md"
+    href="/"
+  >
+    <RoundKeyboardBackspace class="group-hover:translate-x-1 transition-transform" />
+    Back to homepage
+  </a>
+</div>
