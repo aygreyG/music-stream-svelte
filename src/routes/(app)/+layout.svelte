8b28a684b099f84a40aad0db7e03671b84c7e826
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-12-21 23:36:49 +0100

    Remove View Transitions and add basic fade

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import AudioPlayer from '$lib/components/AudioPlayer.svelte';
   import NavBar from '$lib/components/NavBar.svelte';
+  import { fly } from 'svelte/transition';
 
   export let data;
 </script>
@@ -8,7 +9,15 @@
 <div class="flex gap-1 h-[calc(100%-11rem)] pb-1">
   <NavBar user={data.user} />
   <div class="sm:w-8/12 xl:w-9/12 w-full bg-zinc-900/95 rounded-md h-full overflow-hidden">
-    <slot />
+    {#key data.url}
+      <div
+        class="w-full h-full"
+        in:fly={{ opacity: 0, delay: 200, duration: 200 }}
+        out:fly={{ opacity: 0, duration: 200 }}
+      >
+        <slot />
+      </div>
+    {/key}
   </div>
 </div>
 <div class="h-44">
