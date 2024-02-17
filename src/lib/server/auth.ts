03e24790f982a3f0af0ee444f3bca132e29617b0
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2024-02-17 18:35:54 +0100

    Remove include tracks in playlists

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -89,7 +89,6 @@ export async function validateToken(authCookie: string) {
       createdAt: true,
       updatedAt: true,
       playlists: {
-        include: { tracks: true },
         orderBy: { createdAt: 'desc' }
       }
     }
