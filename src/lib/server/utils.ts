d9bb68de1b3fc09ea66f213350a0245366514767
aygreyG <78927537+aygreyG@users.noreply.github.com>
2024-10-26 17:24:59 +0200

    Update to svelte 5 stable Fixes #150

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -27,3 +27,29 @@ export async function getSubFolders(dir: string): Promise<FolderNode[]> {
 
   return subFolders;
 }
+
+export function serverLog(message: string | object, level: 'info' | 'warn' | 'error' = 'info') {
+  const now = new Date();
+  const timestamp =
+    now.toLocaleString('en-GB', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    }) + `.${now.getMilliseconds()}`;
+
+  const colorMap = {
+    info: '\x1b[32m', // Green
+    warn: '\x1b[33m', // Yellow
+    error: '\x1b[31m' // Red
+  };
+  const resetColor = '\x1b[0m';
+
+  const formattedMessage = typeof message === 'object' ? JSON.stringify(message, null, 2) : message;
+  console.log(
+    `${colorMap[level]}[${level.toUpperCase()}]${resetColor}[${timestamp}] ${formattedMessage}`
+  );
+}
