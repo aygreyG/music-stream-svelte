66c6a3bffe64a31d58209a4fceecc80923db29b4
aygreyG <78927537+aygreyG@users.noreply.github.com>
2026-02-28 16:22:04 +0100

    fix: update releases
    * use workflow instead of main branch for stable release

---
+++ release.config.cjs
@@ -0,0 +1,29 @@
+const isStable = process.env.STABLE_RELEASE === 'true';
+
+module.exports = {
+  branches: isStable ? ['main'] : [{ name: 'main', prerelease: 'dev' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml']
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        verifyReleaseCmd: 'echo "version=${nextRelease.version}" >> $GITHUB_OUTPUT',
+        successCmd: 'echo "sha=$(git rev-parse HEAD)" >> $GITHUB_OUTPUT'
+      }
+    ]
+  ]
+};
